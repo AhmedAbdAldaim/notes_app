d0beaff2c07fd8047afb209de0ac6fe33406ab51
AhmedAbdAldaim <ahmedneymar238752@gmail.com>
2023-01-30 05:03:11 -0800

    add edit note

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/note_cubit/note_cubit_cubit.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
+import '../model/note_model.dart';
+
 class EditNoteView extends StatelessWidget {
+  final NoteModel noteModel;
+
   final GlobalKey<FormState> formkey = GlobalKey();
   final TextEditingController title = TextEditingController();
   final TextEditingController des = TextEditingController();
-  EditNoteView({super.key});
+  EditNoteView({super.key, required this.noteModel});
 
   @override
   Widget build(BuildContext context) {
+    title.text = noteModel.title;
+    des.text = noteModel.description;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Title'),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.check))],
+        actions: [
+          IconButton(
+              onPressed: () {
+                noteModel.title= title.text;
+                noteModel.description= des.text;
+                noteModel.save();
+                BlocProvider.of<NoteCubitCubit>(context).featchAllNotes();
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.check))
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -20,20 +38,20 @@ class EditNoteView extends StatelessWidget {
           child: Form(
             key: formkey,
             child: Column(
-              children:  [
-               const SizedBox(
+              children: [
+                const SizedBox(
                   height: 20,
                 ),
                 CustomTextField(
                   controller: title,
                   hint: 'title',
                   validator: (val) {
-                  if (val!.isEmpty) {
-                    return 'empty';
-                  }
-                },
+                    if (val!.isEmpty) {
+                      return 'empty';
+                    }
+                  },
                 ),
-               const SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
                 CustomTextField(
@@ -41,10 +59,10 @@ class EditNoteView extends StatelessWidget {
                   hint: 'description',
                   maxLines: 5,
                   validator: (val) {
-                  if (val!.isEmpty) {
-                    return 'empty';
-                  }
-                },
+                    if (val!.isEmpty) {
+                      return 'empty';
+                    }
+                  },
                 ),
               ],
             ),
